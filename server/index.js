7723a89c2f2770775adcb31586603f67b515662f
Jonathan Redrado <jredrado@dblandit.com>
2024-01-20 21:53:40 -0300

    Configuracion inicial y estructura del servidor

--- server/index.js
+++ server/index.js
@@ -1,11 +1,38 @@
+//IMPORTACION DE DEPENDENCIAS
 const express = require('express')
-const app = express()
-const port = 3000
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+/*
+IMPORTAR RUTAS
+*/
+
+//VARIABLES DE ENTORNO
+const port = 3000
+const uri = "mongodb+srv://admin:vesvl4Qa8anD64OU@preet.ddiecia.mongodb.net/"
+
+//CONEXION A BD
+mongoose.connect(uri).then(()=>{
+  console.log('Conexion exitosa');
+}).catch((error)=>{
+  console.error('Error al conectarse', error);
 })
 
+//INSTANCIA DE EXPRESS
+const app = express()
+
+//MIDDLEWARES
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(cors())
+
+//RUTAS
+app.get('/', (req, res) => {
+  res.send('Welcome to PREET!')
+})
+
+//INICIAR SERVIDOR
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`PREET running on port ${port}`)
 })
