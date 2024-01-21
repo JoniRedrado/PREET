f8c7c5a5764706650b7de3c09d98b280c4cfb7c9
Jonathan Redrado <jredrado@dblandit.com>
2024-01-20 22:02:22 -0300

    GIT config

--- server/index.js
+++ server/index.js
@@ -18,18 +18,6 @@ mongoose.connect(uri).then(()=>{
   console.log('Conexion exitosa');
 }).catch((error)=>{
   console.error('Error al conectarse', error);
-=======
-const bodyParser = require("body-parser")
-
-const app = express()
-const port = process.env.PORT || 3977
-
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World!')
->>>>>>> 42f730a74406c662513b63772ff3dbdef34a04a9
 })
 
 //INSTANCIA DE EXPRESS
